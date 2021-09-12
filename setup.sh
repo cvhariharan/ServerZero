ee79b7bee00200e4ef02318589d5c5441bc88832
Hariharan <14965074+cvhariharan@users.noreply.github.com>
2021-09-12 21:32:26 +0530

    Add setup script

--- setup.sh
+++ setup.sh
@@ -1,1 +1,22 @@
-openssl req -key traefik/certs/example.key -new -x509 -days 365 -out traefik/certs/example.crt
+export $(grep -v '^#' .env | xargs)
+
+if sudo docker network ls | grep web -q
+then
+    echo "Docker network web created"
+else
+    sudo docker network create web
+fi
+
+if [[ -z "$DOMAIN_NAME" ]]; then
+    echo "Please set DOMAIN_NAME"
+    exit 1
+fi
+
+mkdir -p traefik/certs/
+mkdir -p traefik/conf/
+sed "s/DOMAIN_NAME/$DOMAIN_NAME/" traefik/conf/traefik-template.yml > traefik/conf/traefik.yml
+
+
+openssl req -newkey rsa:4096 -x509 -sha256 -days 3650 -nodes -out traefik/certs/$DOMAIN_NAME.crt -keyout traefik/certs/$DOMAIN_NAME.key
+
+unset $(grep -v '^#' .env | sed -E 's/(.*)=.*/\1/' | xargs)
