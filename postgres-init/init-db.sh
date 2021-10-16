1643419482951cfb86ff99e968cef7fcbecd0d4c
Hariharan <14965074+cvhariharan@users.noreply.github.com>
2021-10-16 16:10:37 +0530

    Add paperless, remove nocodb

--- postgres-init/init-db.sh
+++ postgres-init/init-db.sh
@@ -5,5 +5,5 @@ echo $POSTGRES_DB
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE miniflux;
-    CREATE DATABASE nocodb;
+    CREATE DATABASE paperless;
 EOSQL
