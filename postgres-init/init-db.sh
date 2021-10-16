1bc3d2dc750f5a567e7f21c1cbfd86ae680a99c7
Hariharan <14965074+cvhariharan@users.noreply.github.com>
2021-10-16 13:08:57 +0530

    Add miniflux, postgres db creation script

---
+++ postgres-init/init-db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+echo $POSTGRES_DB
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE miniflux;
+    CREATE DATABASE nocodb;
+EOSQL
